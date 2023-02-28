c719a3ddcded6e5e643f61a23211aa7711c92cd2
Dengloo <rle94000@gmail.com>
2023-02-28 04:07:09 -0500

    finished

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ let countEl = document.getElementById("count-el")
 let count = 0
 
 function increment () {
-	count = count + 1
-	countEl.innertext = count
+	count += 1
+	countEl.innerText = count
 
 }
 
 function save () {
 	let countStr = count + " - "
-	saveEl.innerText += countStr
-	console.log(count)
+	saveEl.textContent += countStr
+	countEl.textContent = 0
+	count = 0
 }
